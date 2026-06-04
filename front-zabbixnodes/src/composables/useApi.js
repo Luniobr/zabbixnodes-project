e0a4420db82a2d568caea0f23b19d2684a516d10
Leon de Oliveira <leon.oliveira@luniobr.com>
2026-06-04 11:23:50 -0300

    fix: removendo console.log

--- front-zabbixnodes/src/composables/useApi.js
+++ front-zabbixnodes/src/composables/useApi.js
@@ -22,7 +22,6 @@ client.interceptors.request.use((config) => {
 // Interceptor para logs de resposta
 client.interceptors.response.use(
   (response) => {
-    console.log('API Response:', response.status, response.data)
     return response
   },
   (error) => {
